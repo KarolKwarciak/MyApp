782cdb7bc0e703082973c9c5a3c34b069d232419
KarolKwarciak <kakwarc@gmail.com>
2025-03-16 20:03:37 +0100

    16.03.2025

--- EShop.Application.Tests/UnitTest1.cs
+++ EShop.Application.Tests/UnitTest1.cs
@@ -1,38 +1,57 @@
-namespace EShop.Application.Tests;
+using Xunit;
 using EShop.Application;
 
-    public class UnitTest1
+namespace EShop.Application.Tests
+{
+    public class CreditCardServiceTests
     {
+        private readonly CreditCardService _creditCardService;
+
+        public CreditCardServiceTests()
+        {
+            _creditCardService = new CreditCardService();
+        }
+
         [Theory]
         [InlineData("1234")]
         [InlineData("123456789")]
         [InlineData("3213123")]
-        public void ValidateCard_ReturnFalse_TooShort(string b)
+        public void ValidateCard_ReturnsFalse_WhenTooShort(string cardNumber)
         {
-        var result = ValidateCard(b);
-        Assert.Equal(False, result);
+            var result = _creditCardService.ValidateCard(cardNumber);
+            Assert.False(result);
         }
 
         [Theory]
         [InlineData("12345678912347656765756757657")]
         [InlineData("1234567891234897879789595879789")]
         [InlineData("123456789123454353464564564563546")]
-
-        public void ValidateCard_ReturnFalse_TooLong(string a)
-        { 
-        var result = ValidateCard(a);
-        Assert.Equal(False, result);
-        }
-
-
-    [Theory]
-        [InlineData("1234567891234")]
-        [InlineData("1234567891234567")]
-        [InlineData("12345678912345678")]
-        public void ValidateCard_ReturnTrue(string s)
+        public void ValidateCard_ReturnsFalse_WhenTooLong(string cardNumber)
         {
-        var result = ValidateCard(s);
-        Assert.Equal(True, result);
+            var result = _creditCardService.ValidateCard(cardNumber);
+            Assert.False(result);
         }
-       
+
+        [Theory]
+        [InlineData("4532015112830366")] 
+        [InlineData("6011111111111117")] 
+        [InlineData("378282246310005")]  
+        public void ValidateCard_ReturnsTrue_WhenValidCardNumber(string cardNumber)
+        {
+            var result = _creditCardService.ValidateCard(cardNumber);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("4532015112830366", "Visa")]
+        [InlineData("5105105105105100", "MasterCard")]
+        [InlineData("378282246310005", "American Express")]
+        [InlineData("6011111111111117", "Discover")]
+        [InlineData("3528000000000000", "JCB")]
+        public void GetCardType_ReturnsCorrectType(string cardNumber, string expectedType)
+        {
+            var result = _creditCardService.GetCardType(cardNumber);
+            Assert.Equal(expectedType, result);
+        }
     }
+}
